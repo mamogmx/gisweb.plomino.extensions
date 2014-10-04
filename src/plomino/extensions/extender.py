ffbec091db6b8ed426113921c6e5ce7946ab9757
mamogmx <marco.carbone@gmx.com>
2014-10-04 11:47:18 +0200

    a

--- src/plomino/extensions/extender.py
+++ src/plomino/extensions/extender.py
@@ -13,9 +13,14 @@ from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 
 from Plomino.CMFPlomino.PlominoDatabase import PlominoDatabase
+from Plomino.CMFPlomino.PlominoDocument import PlominoDocument
 
 
-from Products.CMFPlomino.config import READ_PERMISSION
+from Products.CMFPlomino.config import EDIT_PERMISSION
+
+
+from PGReplication import pgReplication
+
 
 class _ExtensionStringField(ExtensionField, StringField): pass
 
@@ -36,8 +41,23 @@ class PlominoExtender(object):
                 description=u"Connection String To a Database",
             ),
         ),
-        
+        _ExtensionTextField(
+            name='dbSchema',
+            widget=StringWidget(
+                label=u"Schema",
+                description=u"Schema",
+            ),
+            default = u'public',
+        ),
+        _ExtensionTextField(
+            name='dbTable',
+            widget=StringWidget(
+                label=u"Table",
+                description=u"table",
+            ),
+        ),
     ]
+    
     def __init__(self, context):
         self.context = context
     
@@ -45,10 +65,55 @@ class PlominoExtender(object):
         return self.fields
         
         
-InitializeClass(PlominoDatabase)        
-        
-def createDoc(self,docid=None):
-    self.createDocument()
-    event.notify(IObjectInitializedEvent)
+InitializeClass(PlominoDocument)        
+PlominoDocument.security = ClassSecurityInfo()   
+     
+PlominoDocument.security.declareProtected(EDIT_PERMISSION, 'saveDocument')
+def saveDoc(self, REQUEST, creation=False):
+    """ Save a document using the form submitted content
+    """
+    db = self.getParentDatabase()
+    form = db.getForm(REQUEST.get('Form'))
 
-PlominoDocument.createDocument=createDoc    
+    errors = form.validateInputs(REQUEST, doc=self)
+
+    # execute the beforeSave code of the form
+    error = None
+    try:
+        error = self.runFormulaScript(
+                SCRIPT_ID_DELIMITER.join(['form', form.id, 'beforesave']),
+                self,
+                form.getBeforeSaveDocument)
+    except PlominoScriptException, e:
+        e.reportError('Form submitted, but beforeSave formula failed')
+
+    if error:
+        errors.append(error)
+
+    # if errors, stop here, and notify errors to user
+    if errors:
+        return form.notifyErrors(errors)
+
+    self.setItem('Form', form.getFormName())
+
+    # process editable fields (we read the submitted value in the request)
+    form.readInputs(self, REQUEST, process_attachments=True)
+
+    # refresh computed values, run onSave, reindex
+    self.save(form, creation)
+    repl = PGReplication(self)
+    repl.saveData()
+    
+    redirect = REQUEST.get('plominoredirecturl')
+    if not redirect:
+        redirect = self.getItem("plominoredirecturl")
+    if type(redirect) is dict:
+        # if dict, we assume it contains "callback" as an URL that will be
+        # called asynchronously, "redirect" as the redirect url (optional,
+        # default=doc url), and "method" (optional, default=GET)
+        redirect = "./async_callback?" + urlencode(redirect)
+    if not redirect:
+        redirect = self.absolute_url()
+    REQUEST.RESPONSE.redirect(redirect)
+
+PlominoDocument.saveDocument=createDoc    
